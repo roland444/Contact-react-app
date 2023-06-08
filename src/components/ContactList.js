947532f87f0e5d55bfdde23e7e029787a7f90cda
roland444 <123905024+roland444@users.noreply.github.com>
2023-06-08 23:03:10 +0200

    Added main components

---
+++ src/components/ContactList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactList = (props) => {
+    return (
+        <div className="ui clled list">
+            Contact List
+        </div>
+    )
+}
+
+export default ContactList;
