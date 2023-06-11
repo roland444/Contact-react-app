3a061131b7565410eea6f2812e448b1e3c3c872a
roland444 <123905024+roland444@users.noreply.github.com>
2023-06-11 22:45:49 +0200

    Added useEffects in App.js

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -8,10 +8,12 @@ class AddContact extends React.Component {
 
     add = (e) => {
         e.preventDefault();
-        if (this.state.name === "" && this.state.email === "") {
+        if (this.state.name === "" || this.state.email === "") {
             alert("All the fields are mandatory!");
             return;
         }
+        this.props.addContactHandler(this.state)
+        this.setState({name: "", email: ""});
     };
 
     render() {
